629fbdb117cf721705376697faa49ec72f44b514
msisaif <msi132043@gmail.com>
2022-08-21 07:35:42 +0600

    Add HTML Template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CIAS</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body class="antialiased">
+        
+        @yield('content')
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
