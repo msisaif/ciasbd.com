df3226961a33385f215d23fde36e406b50f0a757
msisaif <msi132043@gmail.com>
2022-08-23 08:13:12 +0600

    App download link common setup

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -21,10 +21,10 @@
         <!-- Page Banner Start -->
         <div class="page-banner-content">
             <ul class="breadcrumb">
-                <li><a href="#">Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li class="active">About</li>
             </ul>
-            <h2 class="title">About <span>EduLe.</span></h2>
+            <h2 class="title">About <span>Us</span></h2>
         </div>
         <!-- Page Banner End -->
     </div>
@@ -521,59 +521,9 @@
     </div>
 </div>
 <!-- Team Member's End -->
+@endsection
 
-<!-- Download App Start -->
-<div class="section section-padding download-section">
-    <div class="app-shape-1"></div>
-    <div class="app-shape-2"></div>
-    <div class="app-shape-3"></div>
-    <div class="app-shape-4"></div>
-
-    <div class="container">
-        <!-- Download App Wrapper Start -->
-        <div class="download-app-wrapper mt-n6">
-            <!-- Section Title Start -->
-            <div class="section-title section-title-white">
-                <h5 class="sub-title">Ready to start?</h5>
-                <h2 class="main-title">
-                    Download our mobile app. for easy to start your
-                    course.
-                </h2>
-            </div>
-            <!-- Section Title End -->
-
-            <img
-                class="shape-1 animation-right"
-                src="assets/images/shape/shape-14.png"
-                alt="Shape"
-            />
-
-            <!-- Download App Button End -->
-            <div class="download-app-btn">
-                <ul class="app-btn">
-                    <li>
-                        <a href="#"
-                            ><img
-                                src="assets/images/google-play.png"
-                                alt="Google Play"
-                        /></a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            ><img
-                                src="assets/images/app-store.png"
-                                alt="App Store"
-                        /></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- Download App Button End -->
-        </div>
-        <!-- Download App Wrapper End -->
-    </div>
-</div>
-<!-- Download App End -->
-
+@section('footer')
 <!-- Testimonial End -->
 <div class="section section-padding-02 mt-n1">
     <div class="container">
